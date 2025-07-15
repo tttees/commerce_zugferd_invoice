fcc42c8e7962a9b86cce7d5af15fd069847c6fae
tttees <info@hilf-meinem-pc.de>
2025-07-15 10:11:24 +0200

    Create InvoiceController.php

---
+++ src/Controller/InvoiceController.php
@@ -0,0 +1,17 @@
+namespace Drupal\commerce_zugferd_invoice\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Drupal\commerce_order\Entity\Order;
+
+class InvoiceController {
+  public function download($order_id) {
+    $order = Order::load($order_id);
+    $generator = \Drupal::service('commerce_zugferd_invoice.generator');
+    $pdfPath = $generator->generateInvoice($order);
+
+    return new Response(file_get_contents($pdfPath), 200, [
+      'Content-Type' => 'application/pdf',
+      'Content-Disposition' => 'attachment; filename="Rechnung_' . $order_id . '.pdf"',
+    ]);
+  }
+}
