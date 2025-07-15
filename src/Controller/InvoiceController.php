4970a343890152cc2158ba3d058e742c30aa51d4
tttees <info@hilf-meinem-pc.de>
2025-07-15 10:42:01 +0200

    Update InvoiceController.php

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -1,11 +1,16 @@
+<?php
+
 namespace Drupal\commerce_zugferd_invoice\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Drupal\commerce_order\Entity\Order;
 
 class InvoiceController {
+
   public function download($order_id) {
     $order = Order::load($order_id);
+
+    /** @var \Drupal\commerce_zugferd_invoice\InvoiceGenerator $generator */
     $generator = \Drupal::service('commerce_zugferd_invoice.generator');
     $pdfPath = $generator->generateInvoice($order);
 
